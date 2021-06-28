dcf9530afbc92581801d5a57bb3bda935ef1460f
Azilroka <darktemplar9900@yahoo.com>
2021-06-28 16:32:59 -0500

    AddOnSkins Skin

--- ClassTactics/Extras/AddOnSkins/ClassTactics.lua
+++ ClassTactics/Extras/AddOnSkins/ClassTactics.lua
@@ -5,7 +5,7 @@ if not AS then return end
 local CT = unpack(_G.ClassTactics)
 
 function AS:ClassTactics(event, addon)
-	if event == 'PLAYER_ENTERING_WORLD' and IsAddOnLoaded('Blizzard_TalentUI') or addon == 'Blizzard_TalentUI' then
+	hooksecurefunc(CT, 'TalentProfiles', function()
 		AS:SkinBackdropFrame(CT.TalentsFrames)
 		AS:SkinBackdropFrame(CT.TalentsFrames.PvPTalents)
 		AS:SkinButton(CT.TalentsFrames.NewButton)
@@ -16,29 +16,29 @@ function AS:ClassTactics(event, addon)
 		CT.TalentsFrames:SetPoint('TOPLEFT', _G.PlayerTalentFrame, 'TOPRIGHT', 2, -1)
 		CT.TalentsFrames.TitleText:SetFont(CT.Libs.LSM:Fetch('font', 'Expressway'), 12, 'OUTLINE')
 		CT.TalentsFrames.PvPTalents.TitleText:SetFont(CT.Libs.LSM:Fetch('font', 'Expressway'), 12, 'OUTLINE')
+	end)
 
-		hooksecurefunc(CT, "TalentProfiles_CreateLoadout", function()
-			for _, Frame in next, CT.TalentsFrames.Buttons do
-				for _, Button in next, { 'Load', 'Options' } do
-					AS:SkinButton(Frame[Button])
-				end
+	hooksecurefunc(CT, "TalentProfiles_Update", function()
+		for _, Frame in next, CT.TalentsFrames.Buttons do
+			for _, Button in next, { 'Load', 'Options' } do
+				AS:SkinButton(Frame[Button])
 			end
+		end
 
-			for _, Frame in next, CT.TalentsFrames.PvPTalents.Buttons do
-				for _, Button in next, { 'Load', 'Options' } do
-					AS:SkinButton(Frame[Button])
-				end
+		for _, Frame in next, CT.TalentsFrames.PvPTalents.Buttons do
+			for _, Button in next, { 'Load', 'Options' } do
+				AS:SkinButton(Frame[Button])
 			end
-		end)
+		end
+	end)
 
-		hooksecurefunc(CT, "TalentProfiles_CreateExtraButton", function()
-			for _, Button in next, CT.TalentsFrames.ExtraButtons do
-				AS:SkinButton(Button)
-				AS:SkinTexture(Button.icon)
-				AS:SetInside(Button.icon)
-			end
-		end)
-	end
+	hooksecurefunc(CT, "TalentProfiles_CreateExtraButton", function()
+		for _, Button in next, CT.TalentsFrames.ExtraButtons do
+			AS:SkinButton(Button)
+			AS:SkinTexture(Button.icon)
+			AS:SetInside(Button.icon)
+		end
+	end)
 end
 
-AS:RegisterSkin('ClassTactics', AS.ClassTactics, 'ADDON_LOADED')
+AS:RegisterSkin('ClassTactics', AS.ClassTactics)
